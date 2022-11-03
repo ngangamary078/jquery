8c0b5765b2c6361fd31e88c73c984901f5c05b57
Mary <ngangamary078@gmail.com>
2022-11-03 13:40:45 +0300

    erased some spaces

--- js/jquery.js
+++ js/jquery.js
@@ -1,50 +1,29 @@
 $(document).ready(function () {
-
     $(window).resize(function () {
-
         $(".my-btns").css("display", "block");
-
     });
 
     $("#btn-one").click(function () {
-
         alert("Do something");
-
     });
-
-
 
     $("#p-one").click(function () {
-
         $(this).text("updated")//css
-
         // document.getElementById("p-one").innerHTML = "updated";
-
     });
 
-
-
     $(".p-two").click(function () {
-
         alert("Do something for p 2");
-
     });
 
     $("#image").hover(function () {
-
         $(this).mouseenter(function () {
-
             $("#image-span").show()
-
         })
 
         $(this).mouseleave(function () {
-
             $("#image-span").hide();
-
         })
-
-
 
     });
 
